93cfff65f6ddd84285eb88fd830887ad064711dc
Jeff Zohrab <jzohrab@gmail.com>
2022-06-09 14:41:58 -0700

    Extract function.

--- pact-osx-dict.py
+++ pact-osx-dict.py
@@ -1,11 +1,20 @@
 import sys
 import os
+import re
 
 sys.path.append(os.path.abspath(sys.path[0]) + '/vendor')
 from macdict import lookup_word
 
-word = sys.argv[1]
-definition = lookup_word(word)
-if definition:
-    definition = definition.replace('.', '.\n\n')
-print(definition)
+
+def lookup(word):
+    definition = lookup_word(word)
+    if definition:
+        definition = re.sub(r'\.\s+', '.\n\n', definition)
+    return definition
+
+
+if __name__ == '__main__':
+    word = sys.argv[1]
+    d = lookup(word)
+    print('---')
+    print(d)
