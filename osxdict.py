f9f15812cff992e86a38e07169ae4a2c45209a93
Jeff Zohrab <jzohrab@gmail.com>
2022-06-10 14:13:36 -0700

    Remove accents from word search.
    For some reason, the dict search doesn't support accented chars.

--- osxdict.py
+++ osxdict.py
@@ -1,17 +1,23 @@
 import sys
 import os
 import re
+import unicodedata
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "vendor"))
 from macdict import lookup_word
 
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    return only_ascii
 
 def lookup(word):
-    definition = lookup_word(word)
+    w = remove_accents(word).decode("utf-8")
+    print(f'looking up: {w}')
+    definition = lookup_word(w)
     if definition:
-        definition = re.sub(r'\.\s*', '.\n\n', definition)
+        definition = re.sub(r'\.\s+', '.\n', definition)
     return definition
-
 
 if __name__ == '__main__':
     word = sys.argv[1]
